8035ecd6b29f9638ce33371be851a13074aa21c5
pooja197 <47895436+pooja197@users.noreply.github.com>
2019-03-26 11:21:49 -0400

    inital commit

---
+++ HumaneSocietyPets/Controllers/AnimalController.cs
@@ -0,0 +1,218 @@
+﻿using HumaneSocietyPets.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HumaneSocietyPets.Controllers
+
+{
+
+    public class AnimalController : Controller
+
+    {
+
+        private AnimalDBContext db = new AnimalDBContext();
+
+
+
+        // GET: ANIMAL
+
+        public ActionResult Index()
+
+        {
+
+            return View(db.Animals.ToList());
+
+        }
+
+
+
+        // GET: Animal/Create
+
+        public ActionResult Create()
+
+        {
+
+            return View();
+
+        }
+
+
+
+        // POST: Animal/Create
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Animal animal)
+
+        {
+
+            if (ModelState.IsValid)
+
+            {
+
+                db.Animals.Add(animal);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+
+
+            return View(animal);
+
+        }
+
+
+
+        // GET: ANimal/Details/id
+
+        public ActionResult Details(int? id)
+
+        {
+
+            if (id == null)
+
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            Animal animal = db.Animals.Find(id);
+
+            if (animal == null)
+
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(animal);
+
+        }
+
+
+
+        // GET: ANIMAL/Edit/id
+
+        public ActionResult Edit(int? id)
+
+        {
+
+            if (id == null)
+
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            Animal animal = db.Animals.Find(id);
+
+            if (animal == null)
+
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(animal);
+
+        }
+
+
+
+        // POST: ANIMAL/Edit/id
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(Animal animal)
+
+        {
+
+            if (ModelState.IsValid)
+
+            {
+
+                db.Entry(animal).State = EntityState.Modified;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(animal);
+
+        }
+
+
+
+        // GET: ANIMAL/Delete/id
+
+        public ActionResult Delete(int? id)
+
+        {
+
+            if (id == null)
+
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            Animal animal = db.Animals.Find(id);
+
+            if (animal == null)
+
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(animal);
+
+        }
+
+
+
+        // POST: ANIMAL/Delete/id
+
+        [HttpPost]
+
+        [ActionName("Delete")]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult DeleteConfirmed(int id)
+
+        {
+
+           Animal animal = db.Animals.Find(id);
+
+            db.Animals.Remove(animal);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+    }
+
+}
---
+++ HumaneSocietyPets/Models/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HumaneSocietyPets.Models
+{
+    public class Animal
+    {
+        public int AnimalID { get; set; }
+        public string Name { get; set; }
+        public string AnimalType { get; set; } 
+    }
+
+    public class AnimalDBContext:
+        ApplicationDbContext
+    {
+        public DbSet<Animal> Animals
+        {
+            get;set;
+        }
+    }
+}
